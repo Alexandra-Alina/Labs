85afaf4d0126e23a17fcb7d83786fe236024ffda
Alexandra-Alina <96944850+Alexandra-Alina@users.noreply.github.com>
2023-01-11 13:10:02 +0200

    Tema 1

---
+++ Model Tema1/dec.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    //citirea din fisier:
+	ifstream fin("enc.txt"); // folosim tipul de data ifstream pentru intrare
+
+	char c;
+	// citim primul caracter, acesta va fi v
+	fin.get(c);
+	// acesta va fi citit drept un char, iar noi dorim ca acesta sa fie de tip int
+	// in ASCII, cifrele de la 0-9 pornesc de la 48, asa ca:
+	int v = c - '0'; // vezi tabel ASCII
+
+	while(fin.get(c)) // cat timp citim caracter cu caracter din fisier . . .
+        // scriem in consola caracterul decriptat
+		cout << (char)((c - v + 256) % 256);
+
+	return 0;
+}
---
+++ Model Tema1/enc.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    // scrierea in fisier:
+	ofstream fout("enc.txt"); // folosim tipul de data ofstream pentru iesire
+
+	int v; // declaram v de tip int - "cheia" pentru codificare
+	char c; // declaram un caracter c folosit pentru scrierea in fisier
+
+	cout << "Introduceti o valoare pentru cheie: " << endl << "v = ";
+	cin >> v;
+	cin.get(); // sarim peste '\n' (de la cin)
+
+	fout << v; // v va fi primul caracter scris in fisier
+
+	cout << "Introduceti textul care trebuie criptat: " << endl;
+
+	while(cin.get(c)) // cat timp citim caracter cu caracter de la tastatura ...
+    {
+        // daca c va fi newline - iesi din while
+		if(c == '\n') break;
+        // scriem in fisier caracter cu caracter, folosind formula data
+		fout << (char)((c + v) % 256);
+	}
+
+	return 0;
+}
